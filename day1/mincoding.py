e40eaef3d1240ea12579366594662b995c2ded3b
김정환 <jeonghwan916@gmail.com>
2024-01-15 14:35:07 +0900

    day3

--- day1/mincoding.py
+++ day1/mincoding.py
@@ -1,4 +1,9 @@
-a=3
-b=5 #할당
+arr = [0]*6
+a, b = map(int,input().split())
+c=0
+for i in range (a, b+1):
+    arr[c]=i
+    c+=1
 
-#swap 
+for i in range (0, b-a+1):
+    print(arr[i],end='')
--- day3/study.py
+++ day3/study.py
@@ -44,17 +44,22 @@ print(round(0.35,1)) #0.3
 print(round(0.45,1)) #0.5
 print(round(0.55,1)) #0.6
 
+#끝에 작은 수를 더해주면 해결
 print(round(0.5+0.001,1)) #0.5
 print(round(1.5+0.001,1)) #1.5
 print(round(2.5+0.001,1)) #2.5
 print(round(3.5+0.001,1)) #3.5
 print(round(4.5+0.001,1)) #4.5
 
-# math 모듈 사용하기
+#지수 표기법
+print(round(0.5+1e-8,1)) # == 1*10^-8 == 0.00000001
+
+
+# math 모듈 사용하기 (느려짐)
 import math
-print(math.floor(0.5+0.5))
-print(math.floor(1.5+0.5))
-print(math.floor(2.5+0.5))
-print(math.floor(3.5+0.5))
-print(math.floor(4.5+0.5))
-print(math.floor(5.5+0.5))
+print(math.floor(0.5+0.5)) #1
+print(math.floor(1.5+0.5)) #2
+print(math.floor(2.5+0.5)) #3
+print(math.floor(3.5+0.5)) #4
+print(math.floor(4.5+0.5)) #5
+print(math.floor(5.5+0.5)) #6
