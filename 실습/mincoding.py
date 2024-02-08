0980d55f103c8b4ac1c93ec8849e4d5cdcc71a22
김정환 <jeonghwan916@gmail.com>
2024-02-08 14:51:41 +0900

    sleep

--- 실습/mincoding.py
+++ 실습/mincoding.py
@@ -1,21 +1,4 @@
-T = int(input())
-
-for test_ase in range(1, T + 1):
-    bunuck = ["ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"]
-    bt = [0]*10
-    N = list(input().split())
-
-    arr = list(input().split())
-
-    for i in range(10):
-        for j in arr:
-            if bunuck[i] == j:
-                bt[i] += 1
-    print(f'{N[0]}')
-    for i in range(10):
-        if bt[i] >= 1:
-            for j in range(bt[i]):
-                print(bunuck[i], end=' ')
-    print()
-
-
+arr = []
+arr.extend([[1,3]])
+arr.extend([[2,3]])
+print(arr)
